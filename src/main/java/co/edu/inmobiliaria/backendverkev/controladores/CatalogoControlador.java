8cbd3e0617420e8d6c2ccb72849dfe6ec6c88b65
PatoProgramador <skpates18@gmail.com>
2024-06-17 22:08:33 -0500

    Add catalog controller, implementes city and typeId services

--- src/main/java/co/edu/inmobiliaria/backendverkev/controladores/CatalogoControlador.java
+++ src/main/java/co/edu/inmobiliaria/backendverkev/controladores/CatalogoControlador.java
@@ -1,9 +1,12 @@
 package co.edu.inmobiliaria.backendverkev.controladores;
 
+import co.edu.inmobiliaria.backendverkev.dominio.TipoPersona;
+import co.edu.inmobiliaria.backendverkev.dtos.CiudadDTO;
 import co.edu.inmobiliaria.backendverkev.dtos.TipoIdentificacionDTO;
+import co.edu.inmobiliaria.backendverkev.repositorios.TipoPersonaRepository;
+import co.edu.inmobiliaria.backendverkev.servicios.ciudad.CiudadServiceImp;
 import co.edu.inmobiliaria.backendverkev.servicios.tipoIdentificacion.TipoIdentificacionServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping({"/tipoIdentificacion"})
-public class TipoIdentificacionControlador {
+@RequestMapping({"/catalogo"})
+public class CatalogoControlador {
+    @Autowired
+    private CiudadServiceImp ciudadServiceImp;
+
     @Autowired
     private TipoIdentificacionServiceImp tipoIdentificacionServiceImp;
 
-    @GetMapping("/getAll")
-    public List<TipoIdentificacionDTO> getAll() {
+    @GetMapping("/ciudad/listar")
+    public List<CiudadDTO> listarCiudades() {
+        return ciudadServiceImp.listar();
+    }
+
+    @GetMapping("/tipoIdenficacion/listar")
+    public List<TipoIdentificacionDTO> listarTiposIdentificacion() {
         return tipoIdentificacionServiceImp.listar();
     }
+
 }
